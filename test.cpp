b4e12801207034cf8131f85a7d20eaa732ca524b
WOOOOJI <zathy1225@gmail.com>
2023-04-03 14:24:52 +0900

    test commit

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char const *argv[])
+{
+    std::vector<int> arr;
+
+    for(int i = 0; i < 10; i++)
+    {
+        arr.push_back(i);
+    }
+
+    return 0;
+}
